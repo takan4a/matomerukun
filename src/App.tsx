6a7abf4aafe81a664abcb8bc481071b1562b553d
HareyakaYuruyaka <hareyaka16@gmail.com>
2025-09-18 16:06:37 +0900

    モックを増やしました

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@
 import './App.css';
 import moc1 from './assets/moc1.png'; // あなたがアップロードした画像のパスに修正してください
 import moc2 from './assets/moc2.png'; 
+import moc3 from './assets/moc3.png'; 
+import moc4 from './assets/moc4.png'; 
 
 // サンプルデータ
 const posts = [
@@ -16,6 +18,18 @@ const posts = [
     title: 'ローテーション',
     author: '晴れやか',
     date: '3日前',
+  },
+  {
+    image: moc3,
+    title: '筑波大学プロモーションビデオ（施設紹介）/ Get a look into some facilities on Tsukuba Campus',
+    author: '筑波大学',
+    date: '2日前',
+  },
+  {
+    image: moc4,
+    title: '【初心者向け】0から始めるAfter Effects #01 - 画面の見方について -',
+    author: 'Putti Monkey Wrench',
+    date: '1日前',
   },
 
 ];
